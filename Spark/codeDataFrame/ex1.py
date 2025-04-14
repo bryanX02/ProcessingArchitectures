c851caa9d9329a85aab667c7ca81884717a08c9c
dacoleto <dacoleto@ucm.es>
2025-04-14 13:51:57 +0200

    Modificacion ex1 dataframes

--- Spark/codeDataFrame/ex1.py
+++ Spark/codeDataFrame/ex1.py
@@ -3,15 +3,27 @@ import pyspark.sql.functions as F
 import sys
 
 spark = SparkSession.builder.appName("Distributed Grep").getOrCreate()
-search_word = sys.argv[1].lower()
+
+if len(sys.argv) > 1:
+    search_word = sys.argv[1].lower()
+else:
+    raise KeyError("No se ha especificado la palabra a buscar (p.e 'zoroaster')")
+
 df = spark.read.text("./data/input.txt")
 df = df.coalesce(4)
 
-df = df.withColumn("lowerwords", F.split(F.lower(F.col("value")), " "))
+regex_pattern = f"\\b(?i){search_word}\\b"
+
+grep_results = df.filter(F.col("value").rlike(regex_pattern))
+
+grep_results.write.mode("overwrite").text("./Spark/codeDataFrame/output")
+
+
+# df = df.withColumn("lowerwords", F.split(F.lower(F.col("value")), " "))
 
 
 
-df  = df.filter(F.array_contains(F.col("lowerwords"), search_word)).select(F.col("value"))
+# df  = df.filter(F.array_contains(F.col("lowerwords"), search_word)).select(F.col("value"))
 
 
-df.write.text("./Spark/codeDataFrame/output")
+# df.write.text("./Spark/codeDataFrame/output")
